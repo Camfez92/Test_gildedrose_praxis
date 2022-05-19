f0c7475d7107b6f849ba0380d9e7fd65fe1d0c58
Camfez92 <CjfunezG@unal.edu.co>
2022-05-18 21:13:20 -0500

    terraform and jenkins added

---
+++ deploy_ini/main.tf
@@ -0,0 +1,18 @@
+
+module "deploy_ec2" {
+  source        = "../infra"
+  aws_region    = "us-east-1"
+  instance_type = "t2.small"
+  ec2_tags      = { Name = "ec2_test_terraform" }
+  ami_id        = "ami-005de95e8ff495156"
+  key_pair_name = "grupo6-key"
+  user_data     = file("../infra/user_data.sh")
+
+  subnet_id = "subnet-04e972f3a706c00e8"
+  public_ip = true
+
+  sg_name        = "grupo6_ec2"
+  sg_description = "Allow http over port 80 and ssh over port 22"
+  vpc_id         = "vpc-031420f7c99b1a0bd"
+  sg_tags        = { Name = "grupo6_ec2" }
+}
